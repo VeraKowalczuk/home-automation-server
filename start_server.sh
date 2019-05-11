9cf8639f4c2165a7d7b0a718dfedaf8cd6c97463
TillMueller <till@8be.de>
2019-05-11 18:45:51 +0200

    detach from tmux

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
 tmux \
   new-session  "export FLASK_APP=master_server.py; flask run --host=0.0.0.0 --port=5000" \; \
   split-window "export FLASK_APP=slave_server.py; flask run --host=0.0.0.0 --port=4000" \; \
-  select-layout even-vertical
+  select-layout even-vertical \; \
+  detach
