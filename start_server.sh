e0aaf8eaa39da7c9e52981a82d5e3f4a49ad4c40
TillMueller <till@8be.de>
2019-05-11 18:48:50 +0200

    correct paths in start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 tmux \
-  new-session  "export FLASK_APP=master_server.py; flask run --host=0.0.0.0 --port=5000" \; \
-  split-window "export FLASK_APP=slave_server.py; flask run --host=0.0.0.0 --port=4000" \; \
+  new-session  "export FLASK_APP=backend/master_server.py; flask run --host=0.0.0.0 --port=5000" \; \
+  split-window "export FLASK_APP=backend/slave_server.py; flask run --host=0.0.0.0 --port=4000" \; \
   select-layout even-vertical \; \
   detach
