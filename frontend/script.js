4d4650871ba0e8a7c2323b9bbdb8f3f5826f9e07
Vera Kowalczuk <vera.kowalczuk@tum.de>
2021-12-14 02:20:50 +0100

    fix timer input reading and update frontend Co-authored-by: Till Müller <till@8be.de>

--- frontend/script.js
+++ frontend/script.js
@@ -54,25 +54,27 @@ const groupTemplate = ({name, members, plugs}) => `
                             var device_type = "tplink"
                             str1 += `
                             <div class="col-md-6 col-lg-4 col-sm-12 pb-3">
-                                <div class="card-header"> ${ host_name } </div>
-                                <div class="card-body">
-                                    <div class="pb-3">
-                                        <button onclick="tplink_switch('${ device_type }', '${ host_name }', 'on' )" type="button" class="btn btn-secondary">On</button>
-                                        <button onclick="tplink_switch('${ device_type }', '${ host_name }', 'off' )" type="button" class="btn btn-secondary">Off</button>
-                                    </div>
-                                    <h6 class="card-subtitle mb-2">Turn the Light off with a Timer:</h6> 
-                                    <div class="row"> 
-                                    <button onclick="tplink_timer('${ 30*60 }', '${ device_type }', '${ host_name }', 'off')" type="button" class="btn btn-outline-secondary col timerbutton">30 Minutes</button>
-                                    <button onclick="tplink_timer('${ 60*60 }', '${ device_type }', '${ host_name }', 'off')" type="button" class="timerbutton btn btn-outline-secondary col">1 Hour</button>
-                                    <div class="input-group pl-0 col-md-6 col-lg-4 col-sm-12 timerbutton">
-                                        <input type="number" min="5" class="form-control" placeholder="Minutes" id="'${ host_name }'">
-                                        <div class="input-group-append">
-                                            <button onclick="tplink_timer('${-1}', '${ device_type }', '${ host_name }', 'off')" class="btn btn-secondary" type="submit">Set Timer</button>
+                                <div class="card">
+                                    <div class="card-header"> ${ plugs[i].id } </div>
+                                    <div class="card-body">
+                                        <div class="pb-3">
+                                            <button onclick="tplink_switch('${ device_type }', '${ host_name }', 'on' )" type="button" class="btn btn-secondary">On</button>
+                                            <button onclick="tplink_switch('${ device_type }', '${ host_name }', 'off' )" type="button" class="btn btn-secondary">Off</button>
                                         </div>
+                                        <h6 class="card-subtitle mb-2">Turn off with a Timer:</h6> 
+                                        <div class="row"> 
+                                            <button onclick="tplink_timer(${ 30*60 }, '${ device_type }', '${ host_name }', 'off')" type="button" class="btn btn-outline-secondary col timerbutton">30 Minutes</button>
+                                            <button onclick="tplink_timer(${ 60*60 }, '${ device_type }', '${ host_name }', 'off')" type="button" class="timerbutton btn btn-outline-secondary col">1 Hour</button>
+                                            <div class="input-group pl-0 col-md-6 col-lg-4 col-sm-12 timerbutton">
+                                                <input type="number" min="5" class="form-control" placeholder="Minutes" id="${ host_name }">
+                                                <div class="input-group-append">
+                                                    <button onclick="tplink_timer(${-1}, '${ device_type }', '${ host_name }', 'off')" class="btn btn-secondary" type="submit">Set Timer</button>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                </div>
+                            </div>
                             `;
                         }
                         return str1;
@@ -108,14 +110,17 @@ function light_offtimer(group, name, seconds) {
 }
 
 function tplink_switch(device_type, host_name, state) { // state : [on | off]
-    $.post("http://" + window.location.hostname + ":5000/switch/" + device_type + "/" + host_name + "/" + state);
+    $.post("http://" + window.location.hostname + ":4000/switch/" + device_type + "/" + host_name + "/" + state);
 }
 
 function tplink_timer(seconds, device_type, host_name, state) { 
     if (seconds === -1) {
-        seconds = $(this).prev().val() * 60;
+        seconds = document.getElementById(host_name).value * 60;
+        console.log("seconds" + typeof(seconds) + seconds);
+        //console.log("val" + typeof(parseInt($("#" + host_name).val())) + parseInt($("#" + host_name).val()));
+        console.log(document.getElementById(host_name))
     }
-    $.post("http://" + window.location.hostname + ":5000/timer/" + seconds + "/" + device_type + "/" + host_name + "/" + state);
+    $.post("http://" + window.location.hostname + ":4000/timer/" + seconds + "/" + device_type + "/" + host_name + "/" + state);
 }
 
 function sleep(ms) {
